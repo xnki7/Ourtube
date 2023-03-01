7e59395bc520579d24a94d26e8dad3241cd38165
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-03-01 18:59:29 +0530

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import youtube from './api/youtube';
+import { Grid } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <h1>OURTUBE</h1>
+      <Grid style={{justifyContent: "center"}} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            {/* {SearchBar} */}
+          </Grid>
+          <Grid item xs={8}>
+            {/* {VideoDetail} */}
+          </Grid>
+          <Grid item xs={4}>
+            {/* {VideoList} */}
+          </Grid>
+        </Grid>
+      </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/api/youtube.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "https://www.googleapis.com/youtube/v3",
+})
+
+//AIzaSyAB3yO1E0WpPBPxU6iS84gXOX7Os7m1FXQ
